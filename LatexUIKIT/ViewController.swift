89206a9fca7c2383d45ce4284f6bfa0222868d87
Divya Panthi <divya.panthi@innovatetech.co>
2023-09-01 12:57:13 +0545

    Initial Commit

---
+++ LatexUIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LatexUIKIT
+//
+//  Created by Divya Panthi on 01/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
