3f373c46f845c336e20eebf542e63f309a355a8c
Divya Panthi <divya.panthi@innovatetech.co>
2023-09-04 15:56:51 +0545

    using webview.

--- LatexUIKIT/ViewController.swift
+++ LatexUIKIT/ViewController.swift
@@ -6,47 +6,67 @@
 //
 
 import UIKit
-import iosMath
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     
-    lazy var mathLabel: MTMathUILabel = {
-        
-        let label = MTMathUILabel()
-                                                
-        label.latex = "The equation is x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}"
-        
-        label.displayErrorInline = true
-        
-        label.fontSize = 20
-        
-        return label
-        
-    }()
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
+
+        webView = WKWebView()
         
-        view.backgroundColor = .white
-
-        view.addSubview(mathLabel)
-
-        mathLabel.translatesAutoresizingMaskIntoConstraints = false
+        webView.navigationDelegate = self
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(webView)
         
         NSLayoutConstraint.activate([
+        
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             
-            mathLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            mathLabel.trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: -20),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
             
-            mathLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            mathLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             
         ])
+                
+        loadHTMLWithEquations()
         
     }
+    
+    func loadHTMLWithEquations() {
+        
+        let inputString = """
+                It seems that the student is facing some difficulties finding the combined standard deviation. They must remember that if \(latexToMathML(latex: "\\sigma_{2}")) and \(latexToMathML(latex: "\\sigma_{2}")) are the standard derivation of the two series of data with \(latexToMathML(latex: "n1")) and \(latexToMathML(latex: "n2")) number of observations, respectively, then the combined standard deviation of the two series of data is given by \(latexToMathML(latex: "\\sigma_{12}=\\sqrt{\\frac{n_{1} \\sigma_{1}^{2}+n_{2} \\sigma_{2}^{2}+n_{1} d_{1}^{2}+n_{2} d_{2}^{2}}{n_{1}+n_{2}}}")) where \(latexToMathML(latex: "d_{1}=\\overline{x_{1}}-\\overline{x_{12}}")) and \(latexToMathML(latex: "d_{2}=\\overline{x_{2}}-\\overline{x_{12}}"))
+        """
 
+        let htmlContent = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"></script>
+        </head>
+        <body>
+            <p>\(inputString)</p>
+        </body>
+        </html>
+        """
+
+        webView.loadHTMLString(htmlContent, baseURL: nil)
+        
+    }
+    
+    func latexToMathML(latex: String) -> String {
+        // Convert LaTeX to MathML
+        let escapedLatex = latex.replacingOccurrences(of: "\"", with: "\\\"")
+        return "<span class='math' id='math' style='display: inline-block;'>\\( \(escapedLatex) \\)</span>"
+    }
+ 
 }
 
