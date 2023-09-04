49521fafe3580aaa171b3126731039993aa996cd
Divya Panthi <divya.panthi@innovatetech.co>
2023-09-04 09:06:40 +0545

    using ios math

--- LatexUIKIT/ViewController.swift
+++ LatexUIKIT/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import iosMath
 
 class ViewController: UIViewController {
+    
+    lazy var mathLabel: MTMathUILabel = {
+        
+        let label = MTMathUILabel()
+                                                
+        label.latex = "The equation is x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}"
+        
+        label.displayErrorInline = true
+        
+        label.fontSize = 20
+        
+        return label
+        
+    }()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        view.backgroundColor = .white
 
+        view.addSubview(mathLabel)
+
+        mathLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            mathLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            mathLabel.trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: -20),
+            
+            mathLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            mathLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+        ])
+        
+    }
 
 }
 
